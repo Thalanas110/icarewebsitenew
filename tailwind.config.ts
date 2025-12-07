08b7a52ab45b5c735f037029a988fc6c9609157e
Thalanas110 <amdimate43@gmail.com>
2025-12-07 14:15:10 +0800

    zruff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,8 +14,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        display: ['"Playfair Display"', 'serif'],
-        sans: ['Inter', 'sans-serif'],
+        display: ['Poppins', 'sans-serif'],
+        sans: ['Poppins', 'sans-serif'],
       },
       colors: {
         border: "hsl(var(--border))",
