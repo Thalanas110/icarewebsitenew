ff98cfc35e22ce1e6810cb5212324296ef4506fc
Thalanas110 <amdimate43@gmail.com>
2025-12-17 09:49:50 +0800

    added a sitemap

--- server.js
+++ server.js
@@ -55,6 +55,32 @@ export async function createServer(
         );
     }
 
+    app.get('/sitemap.xml', (req, res) => {
+        const protocol = req.protocol;
+        const host = req.get('host');
+        const baseUrl = `${protocol}://${host}`;
+        const urls = [
+            '/',
+            '/about',
+            '/services',
+            '/contact'
+        ];
+
+        const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls.map(url => `
+    <url>
+        <loc>${baseUrl}${url}</loc>
+        <changefreq>weekly</changefreq>
+        <priority>${url === '/' ? '1.0' : '0.8'}</priority>
+    </url>
+    `).join('')}
+</urlset>`;
+
+        res.set('Content-Type', 'text/xml');
+        res.send(sitemap);
+    });
+
     app.use(async (req, res, next) => {
         try {
             const url = req.originalUrl;
